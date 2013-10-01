70d994670dd33a0935fe1ab7e925d62a892ae7ed
mark ptak <mjptak@gmail.com>
2013-09-30 22:03:17 -0500

    changes

--- demo1/__main__.py
+++ demo1/__main__.py
@@ -1,2 +1,9 @@
 #! /lib/python2.6 python
-print "this is just a dummy example file"
+import os
+i = 0
+while i < 5:
+    print "this is just a dummy example file"
+    i+=1
+marklist = os.listdir("/Users/markptak")
+os.rename("/Users/markptak/ptakdemos/demo1/two.txt", "/Users/markptak/ptakdemos/demo1/one.txt")
+print marklist[0]
